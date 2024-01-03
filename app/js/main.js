04a7db1927fee8d83f84d94ea427c5117f4fa6fb
Taras <34415001+TarasKostivi@users.noreply.github.com>
2024-01-03 15:49:49 +0200

    added slider-arrows, logo-app

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
 $(function () {
   $(".slider__inner").slick({
-    arrows: false
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><svg class="slider__arrow-icon"><use xlink:href="images/sprite.svg#icon-arrow"></use></svg></button>',
+    nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><svg class="slider__arrow-icon"><use xlink:href="images/sprite.svg#icon-arrow"></use></svg></button>'
   });
 })
 var mixer = mixitup('.category-content');
+
+
+    
